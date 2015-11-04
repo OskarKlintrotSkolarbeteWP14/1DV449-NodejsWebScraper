74ecc5b8ff4e21df7038609035a4399f9064c13f
Oskar Klintrot <oskar.klintrot@gmail.com>
2015-11-04 11:01:26 +0100

    NodeJS/Express/Typescript project set up according to MVC pattern. Small web scraper example for scraping IMDB added.

---
+++ NodejsWebScraper/models/Scraper.ts
@@ -0,0 +1,47 @@
+﻿import express = require('express');
+import request = require('request');
+import cheerio = require('cheerio');
+
+export class Scraper {
+    private url: string;
+    constructor(url: string) {
+        this.url = url;
+
+        this.scrape();
+    }
+    test() {
+        console.log(this.url);
+    }
+    scrape() {
+        request('http://www.imdb.com/title/tt1229340/', function (error, response, html) {
+            if (!error) {
+                var $ = cheerio.load(html);
+
+                var title, release, rating;
+                var json = { title: "", release: "", rating: "" };
+
+                $('.header').filter(function () {
+                    var data = $(this);
+                    title = data.children().first().text();
+                    release = data.children().last().children().text();
+
+                    json.title = title;
+                    json.release = release;
+
+                    return true;
+                })
+
+                $('.star-box-giga-star').filter(function () {
+                    var data = $(this);
+                    rating = data.text();
+
+                    json.rating = rating;
+
+                    return true;
+                })
+            }
+
+            console.log(json);
+        });
+    }
+}
---
+++ NodejsWebScraper/routes/index.ts
@@ -0,0 +1,23 @@
+﻿import express = require('express');
+import Scraper = require('../models/Scraper');
+
+/*
+ * GET home page.
+ */
+export function index(req: express.Request, res: express.Response) {
+    res.render('index', { title: 'Duschskrapan', year: new Date().getFullYear() });
+};
+
+export function about(req: express.Request, res: express.Response) {
+    res.render('about', { title: 'Om sidan', year: new Date().getFullYear(), message: '' });
+};
+
+
+/*
+ * POST home page.
+ */
+export function scrape(req: express.Request, res: express.Response) {
+    var scraper = new Scraper.Scraper(req.body.urlToScrape);
+    scraper.test();
+    index(req, res);
+};
