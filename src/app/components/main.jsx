910260da2e37ef81299420ecb2eaa0128f780a26
OskarKlintrot <oskar.klintrot@gmail.com>
2015-12-09 18:20:18 +0100

    Testing XHR on gh-pages

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 const Main = () => {
+  jQuery.getJSON( "http://api.sr.se/api/v2/traffic/messages?format=json&pagination=false",
+  (data)=> {
+    console.log(data);
+  } );
+
   return(
     <div>
       <h1>
