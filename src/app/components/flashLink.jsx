ea8bb9ff2cf542d7407301e22408f928e6bfcdee
OskarKlintrot <oskar.klintrot@gmail.com>
2015-12-14 10:32:31 +0100

    Refactored from jQuery -> React

---
+++ src/app/components/flashLink.jsx
@@ -0,0 +1,52 @@
+import React, { PropTypes } from 'react';
+import LeafletMap, { LeafletSettings } from '../scripts/leafletMap';
+import { FormatDate } from '../scripts/helper';
+import { Categories } from '../scripts/viewModel';
+import Markers from '../scripts/markers';
+
+const FlashLink = React.createClass({
+  propTypes: {
+    data: React.PropTypes.array.isRequired
+  },
+
+  renderList(){
+    let list = [];
+    this.props.data.map((message) => {
+      let exactlocation = message.feature.properties.exactlocation ? ', ' + message.feature.properties.exactlocation : '';
+      let description = message.feature.properties.description ? message.feature.properties.description : '';
+
+      list.push(
+        <a href="#" className="list-link" title={message.feature.properties.title} onClick={(e) => {
+            const marker = Markers.Map[message.feature.id];
+            marker.openPopup();
+            e.preventDefault();
+        }}>
+          <div>
+            <strong>
+              {Categories[message.feature.properties.category] + ', ' + message.feature.properties.subcategory + ': '}
+            </strong>{message.feature.properties.title + exactlocation}
+          </div>
+          <div>
+            {description}
+          </div>
+          <div>
+            {FormatDate(message.feature.properties.createddate)}
+          </div>
+          <br />
+        </a>
+      );
+    });
+    return list;
+  },
+
+  render() {
+    let list = this.renderList();
+    return(
+      <div>
+        {list}
+      </div>
+    );
+  }
+});
+
+export default FlashLink;
--- src/app/components/listMessages.jsx
+++ src/app/components/listMessages.jsx
@@ -1,57 +1,42 @@
 import React from 'react';
-import { JsonDateToDate, FormatDate } from '../scripts/helper';
-import { Categories } from '../scripts/viewModel';
+import FlashLink from './flashLink';
+// import { JsonDateToDate, FormatDate } from '../scripts/helper';
+// import { Categories } from '../scripts/viewModel';
 import Markers from '../scripts/markers';
 
-const styles = {
-  markers: {
-    // height: '150px',
-    // position: 'relative',
-    // paddingBottom: '50px',
-    // overflow: 'auto',
-  },
-  listcontainer: {
-
-  }
-};
-
 const ListMessages = React.createClass({
-  componentDidMount() {
-    const addToList = (message) => {
-      let exactlocation = message.feature.properties.exactlocation ? ', ' + message.feature.properties.exactlocation : '';
-      let description = message.feature.properties.description ? message.feature.properties.description : '';
-      $('div#markers').append(
-        '<a href="#" class="list-link" data-message-id=\"'+message.feature.id+'\" title="' + message.feature.properties.title + '"><div class="info-list-item">' + '<div class="info-list-txt">' + '<div class="title"><strong>' + Categories[message.feature.properties.category] + ', ' + message.feature.properties.subcategory + ': </strong>' + message.feature.properties.title + exactlocation + '</div>' + '<div>' + description + '</div>' + '<div>' + FormatDate(message.feature.properties.createddate) + '</div>' + '</div>' + '</div>' + '</a>' + '<br />'
-      );
+  getInitialState(){
+    return{
+      data: null
     };
+  },
 
-    for (const item of Markers.Sorted()) {
-      addToList(item);
-    }
-    // for (const item in Markers.Map) {
-    //   addToList(Markers.Map[item]);
-    // }
-
-    $('a.list-link').click(function (e) {
-        const messageId = $(this).data('message-id');
-        const marker = Markers.Map[messageId];
-        marker.openPopup();
-        // marker.openPopup(marker.getLatLng());
-
-        e.preventDefault();
-    });
-
+  componentDidMount() {
+    this.setState({data: Markers.Sorted()});
   },
 
   render() {
-    return (
-      <div>
-        <h4>Händelser</h4>
-        <div id="listcontainer" style={styles.listcontainer}>
-          <div id="markers" style={styles.markers}></div>
+    if(this.state.data) {
+      return (
+        <div>
+          <h4>Händelser</h4>
+          <div id="listcontainer">
+            <div id="markers">
+              <FlashLink data={this.state.data} />
+            </div>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div>
+          <h4>Händelser</h4>
+          <div id="listcontainer">
+            <div id="markers"><h3>Laddar...</h3></div>
+          </div>
+        </div>
+      );
+    }
   }
 });
 
